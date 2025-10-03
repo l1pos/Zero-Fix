c46ccac1f69b6ad34cd527f34da0d17caa95b737
l1pos <igorchigh@gmail.com>
2025-10-03 14:45:09 +0300

    fix

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Zero-Fix/', // 👈 ОБЯЗАТЕЛЬНО! имя репозитория
+  build: {
+    outDir: 'dist',  // Папка сборки
+  },
 })
